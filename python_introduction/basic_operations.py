7b8a997ff2729a8071766e8395666eb499e2c5ff
Chris Zantira <chrizantira@gmail.com>
2024-09-10 23:02:18 +0000

    Add basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("Addition of 10 and 5 is ", number1 + number2)
+print("Subtraction of 10 and 5 is ", number1 - number2)
+print("Multiplication of 10 and 5 is ", number1 * number2)
