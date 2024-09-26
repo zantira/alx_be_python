3e018241206ff146b9c261a7242ec0332b3fe475
Chris Zanitra <chrizantira@gmail.com>
2024-09-26 15:13:51 +0000

    Added function to perform arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+#In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
+
+def perfom_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation"
