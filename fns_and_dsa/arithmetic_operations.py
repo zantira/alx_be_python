d530b9b736c320d27c5f6836d51dbf1a1eea8239
zantira <chrizantira@gmail.com>
2024-09-26 15:36:16 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,5 @@
-#In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
+#In this script, define a function that performs basic arithmetic operations. 
+#This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
 
 def perform_operation(num1, num2, operation):
     if operation == "add":
