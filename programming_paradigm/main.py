54cd984690250b15f49f29caac9fe124a8730226
Chris Zanitra <chrizantira@gmail.com>
2024-10-04 17:01:20 +0000

    update robust division calculator

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -15,7 +15,7 @@ def main():
 
     result = safe_divide(numerator, denominator)
     if result is not None:
-        print(result)
+        print(f"The result of the division is {result}")
 
 if __name__ == "__main__":
     main()
