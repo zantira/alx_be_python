93fa5e3415deed1e032fff009b43cf969b6c9285
Chris Zanitra <chrizantira@gmail.com>
2024-10-06 17:06:57 +0000

    update tests run

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,60 +6,60 @@ from simple_calculator import SimpleCalculator
 class Runtest(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
         self.a = 4
         self.b = 2
 
     def test_addition_postive(self):
-        result = self.calculator.add(self.a, self.b)
+        result = self.calc.add(self.a, self.b)
         self.assertEqual(result, 6)
 
     def test_add_negative(self):
-        result = self.calculator.add(-self.a, self.b)
+        result = self.calc.add(-self.a, self.b)
         self.assertEqual(result, -2)
 
     def test_add_negative_negative(self):
-        result = self.calculator.add(-self.a, -self.b)
+        result = self.calc.add(-self.a, -self.b)
         self.assertEqual(result, -6)
 
     #unittest for subract() function
     
     def test_subtract_positive(self):
-        result = self.calculator.subtract(self.a , self.b)
+        result = self.calc.subtract(self.a , self.b)
         self.assertEqual(result, 2)
 
     def test_subtract_negative(self):
-        result = self.calculator.subtract(-self.a, self.b)
+        result = self.calc.subtract(-self.a, self.b)
         self.assertEqual(result, -6)
 
     def test_subtract_negative_negative(self):
-        result = self.calculator.subtract(-self.a, -self.b)
+        result = self.calc.subtract(-self.a, -self.b)
         self.assertEqual(result, -2)
 
 
     #unittest for multiply() function
 
     def test_multiply_positive(self):
-        result = self.calculator.multiply(self.a , self.b)
+        result = self.calc.multiply(self.a , self.b)
         self.assertEqual(result, 8)
     
     def test_multiply_negative(self):
-        result = self.calculator.multiply(-self.a, self.b)
+        result = self.calc.multiply(-self.a, self.b)
         self.assertEqual(result, -8)
 
 
     #unittest for the divide() function
 
     def test_divide_positive(self):
-        result = self.calculator.divide(self.a, self.b)
+        result = self.calc.divide(self.a, self.b)
         self.assertEqual(result, 2)
 
     def test_divide_negative(self):
-        result  = self.calculator.divide(-self.a, self.b)
+        result  = self.calc.divide(-self.a, self.b)
         self.assertEqual(result, -2)
 
     def test_divide_zero_division(self): 
-        result = self.calculator.divide(self.a, self.b)
+        result = self.calc.divide(self.a, self.b)
         if self.b == 0:
             self.assertEqual(result, "Cannot divide by zero")
 
