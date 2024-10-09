32e025b94ec85d03b04dccb5c53710996f598b4c
Chris Zanitra <chrizantira@gmail.com>
2024-10-09 15:47:28 +0000

    update book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,7 +9,7 @@ class Book:
 
 
     def __str__(self):
-        return f"{self.title} by {self.author} published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
 
     def __repr__(self):
