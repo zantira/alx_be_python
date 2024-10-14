bf5e13c41c538cf8a540aa8221735555a5452f9d
Chris Zanitra <chrizantira@gmail.com>
2024-10-14 13:43:11 +0000

    update library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -40,3 +40,41 @@ class Library:
                 print(f"Printbook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print(f"Book: {book.title} by {book.author}")
+
+
+
+
+
+
+
+from library_system import Book, EBook, PrintBook, Library
+
+def main():
+    # Create a Library instance
+    my_library = Library()
+
+    # Create instances of each type of book
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+    # Add books to the library
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    # List all books in the library
+    my_library.list_books()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,22 +1,22 @@
-import sys
+# import sys
 
-from robust_division_calculator import safe_divide
+# from robust_division_calculator import safe_divide
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage python main.py <numerator> <denominator>")
-        sys.exit(1)
+# def main():
+#     if len(sys.argv) != 3:
+#         print("Usage python main.py <numerator> <denominator>")
+#         sys.exit(1)
     
-        numerator = float(sys.argv[1])
-        denominator = float(sys.argv[2])
+#         numerator = float(sys.argv[1])
+#         denominator = float(sys.argv[2])
     
-        sys.exit(1)
+#         sys.exit(1)
 
-    result = safe_divide(numerator, denominator)
-    if result is not None:
-        print(f"The result of the division is {result}")
+#     result = safe_divide(numerator, denominator)
+#     if result is not None:
+#         print(f"The result of the division is {result}")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
